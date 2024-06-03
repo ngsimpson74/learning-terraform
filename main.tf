ef97decad02266864e7b44ec0a3521eaf3286670
ngsimpson74 <170197492+ngsimpson74@users.noreply.github.com>
2024-06-03 16:32:21 -0500

    vars 3

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["var.ami_filter.name"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = [var.ami_filter.owner] # Bitnami
-}
-
 data "aws_vpc" "default" {
   default = true
 }
@@ -22,10 +6,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "var.environment.name"
-  cidr = "${var.environment.network_prefix}.0.0/16"
+  cidr = "${var.environment.network_prefix}.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["${var.environment.network_prefix}.0.101.0/24", "${var.environment.network_prefix}.0.102.0/24", "${var.environment.network_prefix}.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
     tags = {
     Terraform = "true"
@@ -36,7 +20,7 @@ module "blog_vpc" {
 module "autoscaling" {
   source    = "HDE/autoscaling/aws"
   version   = "6.4.0"
-  name      = "${var.environment.name}blog"
+  name      = "${var.environment.name}-blog"
   min_size  = var.asg_min_size
   max_size  = var.asg_max_size
 
@@ -98,41 +82,4 @@ module "blog_secgrp" {
   
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_in" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
