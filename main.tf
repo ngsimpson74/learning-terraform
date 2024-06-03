edb4fd64cbdec738708855743b9d88187446fcd9
ngsimpson74 <170197492+ngsimpson74@users.noreply.github.com>
2024-06-03 17:20:55 -0500

    vars 12

--- main.tf
+++ main.tf
@@ -2,6 +2,20 @@ data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_ami" "app_ami" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["bitnami-tomcat-*-x86-hvm-ebs-nami"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [hvm]
+  }
+  owners = ["979382823631"] #bitnami  
+}
+
+
 resource "aws_instance" "blog" {
   ami             = data.aws_ami.app_ami.id
   instance_type   = var.instance_type
