2a89827daf0e1d5abe325b8274cc744a97f32506
ngsimpson74 <170197492+ngsimpson74@users.noreply.github.com>
2024-06-03 16:27:22 -0500

    vaiables 2

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_filter" {
   }
 }
 
-variable = "Environment"{
+variable "Environment" {
   description = "dev env"
 
   type = object ({
@@ -31,22 +31,6 @@ variable = "Environment"{
   }
 }
 
-
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-    tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
 variable "asg_min_size" {
   description = "Min num of instances in the ASG"
   default = 1
