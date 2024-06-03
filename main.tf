578c309a825c411fc34e055a58ed7570acd3af06
ngsimpson74 <170197492+ngsimpson74@users.noreply.github.com>
2024-06-03 17:54:39 -0500

    vars 20

--- main.tf
+++ main.tf
@@ -13,6 +13,22 @@ module "blog_vpc" {
   }
 }
 
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm]
+  }
+
+  owners = ["979382823631] #bitnami
+}
+
 module "autoscaling" {
   source    = "HDE/autoscaling/aws"
   version   = "6.4.0"
