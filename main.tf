0a30a85919a0a8673d5cde72ecdc8b48dc3b424b
ngsimpson74 <170197492+ngsimpson74@users.noreply.github.com>
2024-06-03 16:39:13 -0500

    vars 5

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "blog" {
   ami             = data.aws_ami.app_ami.id
   instance_type   = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog_secgrp.id]
 
   tags = {
     Name = "Learning Terraform"
