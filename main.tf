d4df83d5392f26679ea803ef0b5505c1c9bd45ff
ngsimpson74 <170197492+ngsimpson74@users.noreply.github.com>
2024-06-03 15:06:55 -0500

    creeate new dev vpc

--- main.tf
+++ main.tf
@@ -18,13 +18,31 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+    tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_secgrp.security_group_id]
 
-  tags = {
+  subnet_id = module.blog_vpc.public_subnets[0]
+  
+    tags = {
     Name = "HelloWorld"
   }
 }
@@ -32,11 +50,11 @@ resource "aws_instance" "blog" {
 module "blog_secgrp" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
+  
+  vpc_id = module.blog_vpc.vpc_id
   name    = "blog_secgrp"
   
-  vpc_id = data.aws_vpc.default.id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+    ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   
   egress_rules        = ["all-all"]
